175fca6dde865550217a985e69a77227402dbb9b
swapnil-shah <swapnilshah.me@gmail.com>
2021-02-19 19:39:50 -0500

    constructed folders

---
+++ src/components/signin-btn/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function SignInBtn() {
+  return (
+    <div className="signInBtn">
+      <p>Sign In with Google</p>
+    </div>
+  )
+}
+
+export default SignInBtn
---
+++ src/pages/home/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { SignInBtn } from '../../components'
+
+function Home() {
+  return (
+    <div className="home">
+      <SignInBtn />
+    </div>
+  )
+}
+
+export default Home
